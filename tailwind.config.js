2086386eae65b9d0475aba3419af36d562d50ab0
Tahreem Fatima <tahreemf362@gmail.com>
2025-09-06 12:31:34 +0500

    Add Tailwind config for Vercel build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,8 @@
-module.exports = {
-content: [
-'./index.html',
-'./src/**/*.{js,jsx,ts,tsx}'
-],
-theme: {
-extend: {
-colors: {
-brand: {
-50: '#fff7fb',
-100: '#ffeaf5',
-300: '#ffb3d9',
-500: '#ff6fa9',
-700: '#e0467a'
-}
-},
-fontFamily: {
-sans: ['Inter', 'ui-sans-serif', 'system-ui']
-}
-}
-},
-plugins: [],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
